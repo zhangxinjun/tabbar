b20dac194441dccc32182b17813fa089b3509a6a
zhangxinjun <759561217@qq.com>
2020-03-23 22:07:11 +0800

    使用插槽将基本布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
+
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <tabBar>
+      <tabbarItem>
+        <!-- 使用具名插槽，插入对应的内容 -->
+        <img slot="tabbar-icon" src="./assets/img/4G1_01.png" alt />
+        <div slot="tabbar-text">首页</div>
+      </tabbarItem>
+       <tabbarItem>
+        <img slot="tabbar-icon" src="./assets/img/4Gkuandai_07.jpg" alt />
+        <div slot="tabbar-text">详情</div>
+      </tabbarItem>
+       <tabbarItem>
+        <img slot="tabbar-icon" src="./assets/img/4Gkuandai_14.jpg" alt />
+        <div slot="tabbar-text">购物车</div>
+      </tabbarItem>
+       <tabbarItem>
+        <img slot="tabbar-icon" src="./assets/img/4Gkuandai_17.jpg" alt />
+        <div slot="tabbar-text">我的</div>
+      </tabbarItem>
+
+    </tabBar>
   </div>
 </template>
 
+<script>
+import tabBar from "./component/tabbar/tabBar";
+// 引入item并使用
+import tabbarItem from "./component/tabbar/tabBarItem";
+export default {
+  name: "App",
+  components: {
+    tabBar,
+    tabbarItem
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+/* 引入清除默认样式 */
+@import "./assets/css/reset.css";
 </style>
+
---
+++ src/component/tabbar/tabBarItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tabbar-item">
+      <!-- 使用具名插槽，使之可以插入对应的内容 -->
+    <slot name="tabbar-icon"></slot>
+    <slot name="tabbar-text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabBarItem"
+};
+</script>
+
+<style>
+.tabbar-item {
+  flex: 1;
+}
+.tabbar-icon {
+  vertical-align: middle;
+  margin-top: 3px;
+}
+</style>
